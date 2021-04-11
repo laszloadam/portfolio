7d1c8addb39e3893c1ca0a20a57fc6785c371bcd
László Ádám <laszloadam08@gmail.com>
2021-04-11 17:05:54 +0200

    index:update, css:add.btn, js:add.description

--- others/konyvtar/js/biblio.js
+++ others/konyvtar/js/biblio.js
@@ -5,7 +5,7 @@ const biblio = [
         author: 'C.S. Lewis',
         publication: 1950,
         image: '../konyvtar/pics/narnia2.jpg',
-        description:'Narnia sztori',
+        description:'Narnia egy jéggé dermesztett ország, ahol sohasem jár a Mikulás és nem létezik a karácsony. A négy testvér - Peter, Susan, Edmund és Lucy - egy vidéki kastélyban álló ruhásszekrényen keresztül lép be a birodalomba, hogy felvegye a harcot a Fehér Boszorkánnyal és csatlósaival.... ',
         rating: 4
     },
     {
@@ -14,7 +14,7 @@ const biblio = [
         author: 'Stephani Perry',
         publication: 1999,
         image: '../konyvtar/pics/avpWar.jpg',
-        description: 'AVP sztori',
+        description: 'Machiko Noguchi már egy teljes éve él a vadászok közt, de még mindig úgy érzi, hogy társai nem tartják egyenrangú félnek. Bármit tesz, jutalma büntetés és megvetés. Tudja, nem maradhat sokáig a jautják között.Lara, Jess és Ellis, a 949-es állomáson történt mészárlás túlélői, egy mentőkabinban fuldokolva sodródnak az űrben. Már csak perceik vannak hátra az életből, amikor a Weyland-Yutani társaság megmenti őket. A csapatot egy perembolygóra szállítják. A ragadozók hajója ismét pályára áll egy világ körül, hogy elhintse rajta a halál magvait. A vadászat izgalmasnak ígérkezik, hiszen egyszerre akarnak megküzdeni az univerzum két legveszélyesebb prédájával, az idegenekkel és az emberekkel. Machiko kénytelen meghozni élete legnehezebb döntését, és harcba száll - ezúttal az emberek oldalán, az idegenek és a ragadozók ellen.',
         rating: 5
     },
     {
@@ -23,8 +23,8 @@ const biblio = [
         author: 'J. K. Rowling',
         publication: 1997,
         image: '../konyvtar/pics/hp1.jpg',
-        description: 'HP sztori',
-        rating: 5
+        description: 'A Roxfort Boszorkány- és Varázslóképző Szakiskolában töltött első tanév kemény erőpróba a diákok számára. Harry Potternek nem csupán a vizsgákon kell megfelelnie, de egy életre-halálra szóló küzdelemnek is részese lesz. A tizenegy éves varázslójelölt története meghódította az egész világot.',
+        rating: 2
     }
 ];
 
@@ -32,15 +32,6 @@ const biblio = [
 // kiszámolja, hogy hány könyv van.
 let booksNumber = biblio.length;
 
-
-// könyv osztályozása
-
-/*var rating = "",
-    stars;
-for (let star = 1; star <= bookCatalog.myBook1.rating; star++) {
-    rating = rating + "✰";
-}
-*/
 
 //könyvek kiiratása
 
@@ -53,22 +44,33 @@ for (let x=0; x <= booksNumber-1; x++) {
     subDiv.setAttribute('id', 'div-'+x);
     makeDiv.setAttribute('onmouseover', 'mouseOver(this)');
     makeDiv.setAttribute('onmouseout', 'mouseOut(this)');
-    
-    
 
     main.appendChild(makeDiv);
     makeDiv.style.backgroundImage = "url('"+biblio[x].image+"')";
-    makeDiv.appendChild(subDiv).innerHTML = biblio[x].title +'<br>'+  biblio[x].author +'<br>'+  biblio[x].description +'<br>';
+
+    makeDiv.appendChild(subDiv).innerHTML = '<h3>'+biblio[x].title +'</h3><p>'+biblio[x].author+'</p><p>'+  biblio[x].publication +'</p><p>Vélemény:</p>';
+    for(let star =1; star <= biblio[x].rating; star++ ){
+        subDiv.innerHTML += '✰';
+    };
+    subDiv.innerHTML += '<button class="btns" id="subDiv-btns'+x+'">Részletek</button>';
+    
+     document.getElementById('subDiv-btns'+x).setAttribute('onclick', 'showDescription('+x+')')
+
+    function showDescription(xDesc){
+        alert(biblio[xDesc].description)
+    }
     
 };
 
+//Rating: Annyi csillag jelenjen meg, amennyi a pontja.
 
 var content = document.getElementById('content');
 
-function writerate(){
-    content.write("valami2")
-};
 
+
+
+
+// Show/Hide funkció
 
 function mouseOver(xDiv) {
     xDiv.getElementsByTagName('div')[0].setAttribute('class', 'allDiv show')
