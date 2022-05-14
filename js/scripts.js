a48a2d189f7e85566f9cc306d1d7619ff468d118
laszlo ADam <laszloadam08@gmail.com>
2022-05-14 18:24:44 +0200

    tehem save to localStorage

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+//Téma szín
+
+let theme = window.localStorage.getItem('theme');
+console.log(theme);
+
+if(theme != null) {
+    document.documentElement.style.setProperty('--mainColor', theme)
+}
+else {
+    document.documentElement.style.setProperty('--mainColor', '#18bb34');
+    //document.documentElement.style.setProperty('--secondColor', '#008000');
+}
 
 //pontozás a címben
 
@@ -26,17 +38,20 @@ function optClose() {
 //document.documentElement.style.setProperty('--your-variable', '#YOURCOLOR');
 
 function blue() {
+    window.localStorage.setItem('theme', '#09a8d8')
     document.documentElement.style.setProperty('--mainColor', '#09a8d8');
     document.documentElement.style.setProperty('--secondColor', '#137998');
     document.getElementById("content").style.backgroundImage = "url('pics/cont_bg_blue.png')";
     
 };
 function red() {
+    window.localStorage.setItem('theme', '#ff1f1f')
     document.documentElement.style.setProperty('--mainColor', '#ff1f1f');
     document.documentElement.style.setProperty('--secondColor', '#cc0000');
     document.getElementById("content").style.backgroundImage = "url('pics/cont_bg_red.png')";
 };
 function green() {
+    window.localStorage.setItem('theme', '#18bb34')
     document.documentElement.style.setProperty('--mainColor', '#18bb34');
     document.documentElement.style.setProperty('--secondColor', '#008000');
     document.getElementById("content").style.backgroundImage = "url('pics/cont_bg_green.png')";
